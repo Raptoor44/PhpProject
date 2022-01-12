4500e7b72004c402b36f366488bfe9f07d8362ed
ClementF53 <59557013+ClementF53@users.noreply.github.com>
2022-01-12 15:00:21 +0100

    TRAVAIL

--- poo/Absence.php
+++ poo/Absence.php
@@ -14,36 +14,41 @@ private $comment ; // Comment
 
 public function __construct($absenceDate, $lateTime, $excused, $courseType, $courseDurationMinutes,$id,$comment){
 
-    $this->$absenceDate = $absenceDate;
-    $this->$lateTime = $lateTime;
-    $this->$excused = $excused;
-    $this->$courseType = $courseType;
-    $this->$courseDurationMinutes = $courseDurationMinutes;
-    $this->$id = $id;
-    $this->$comment = $comment;
+    $this->absenceDate = $absenceDate;
+    $this->lateTime = $lateTime;
+    $this->excused = $excused;
+    $this->courseType = $courseType;
+    $this->courseDurationMinutes = $courseDurationMinutes;
+    $this->id = $id;
+    $this->comment = $comment;
 }
 
 public function _getAbsenceDate(){
+    return $this->absenceDate;
 
 }
 public function _getLateTime(){
+    return $this->lateTime;
     
 }
 public function _isExcused(){
+    return $this->excused;
     
 }
 public function _getCourseType(){
-    
+    return $this->courseType;
 }
 public function _getCourseDurationMinutes(){
-    
+    return $this->courseDurationMinutes;
 }
 public function _getComment(){
+    return $this->comment;
     
 }
 
-
-
+public function _setComment($comment){
+     $this->comment = $comment;
+}
 
 }
 ?>
