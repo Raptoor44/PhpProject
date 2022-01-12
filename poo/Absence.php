756fd9e5606e8bfb83595f5471e234d5a5cb1e36
ClementF53 <59557013+ClementF53@users.noreply.github.com>
2022-01-12 14:52:49 +0100

    salut c moi tchoupi

--- poo/Absence.php
+++ poo/Absence.php
@@ -9,17 +9,38 @@ private $excused; //boolean
 private $id; //long
 private $courseType; //String
 private $courseDurationMinutes; //int
+private $comment ; // Comment
 
 
-public function __construct($absenceDate, $lateTime, $excused, $courseType, $courseDurationMinutes){
+public function __construct($absenceDate, $lateTime, $excused, $courseType, $courseDurationMinutes,$id,$comment){
 
     $this->$absenceDate = $absenceDate;
     $this->$lateTime = $lateTime;
     $this->$excused = $excused;
     $this->$courseType = $courseType;
     $this->$courseDurationMinutes = $courseDurationMinutes;
+    $this->$id = $id;
+    $this->$comment = $comment;
 }
 
+public function _getAbsenceDate(){
+
+}
+public function _getLateTime(){
+    
+}
+public function _isExcused(){
+    
+}
+public function _getCourseType(){
+    
+}
+public function _getCourseDurationMinutes(){
+    
+}
+public function _getComment(){
+    
+}
 
 
 
