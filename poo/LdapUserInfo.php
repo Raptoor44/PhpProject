983749c3f2ea1da7bd4fe9b4cd77084e391c9d6f
HugoM53 <59557186+HugoM53@users.noreply.github.com>
2022-01-12 16:26:10 +0100

    Add files via upload

--- poo/LdapUserInfo.php
+++ poo/LdapUserInfo.php
@@ -5,12 +5,24 @@ class LdapUserInfo{
     private $manuallyUpdated; //boolean
     private $ldapSynchronized; //boolean
 
-    public function __construct($lastLdapUpdate,$manuallyUpdated,$ldapSynchronized){
+    public function __construct($lastLdapUpdate,$manuallyUpdated,$ldapSynchronized){ 
         $this->lastLdapUpdate=$ldapSynchronized;
         $this->manuallyUpdated=$manuallyUpdated;
         $this->ldapSynchronized=$ldapSynchronized;
 
 
+    }
+
+    public function getLastLdapUpdate(){
+        return $this->lastLdapUpdate;
+    }
+
+    public function getManuallyUpdated(){
+        return $this->manuallyUpdated;
+    }
+
+    public function getLdapSynchronized(){
+        return $this->ldapSynchronized;
     }
 
 }
